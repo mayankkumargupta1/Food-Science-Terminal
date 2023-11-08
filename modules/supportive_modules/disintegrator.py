10dfa88dbd1bc467a8aded344cb9f2d86ebf90bc
ByteMasterMayank <83159753+mayank-crypto-byte@users.noreply.github.com>
2023-11-08 11:39:47 +0530

    comit

--- modules/supportive_modules/disintegrator.py
+++ modules/supportive_modules/disintegrator.py
@@ -4,5 +4,23 @@ class break_input:
 
     def get(self):
         x = self.input
+        x = x.split('>')
         query = ''
-        
+        protein = ''
+        fats = ''
+        carbohydrates = ''
+        calories = ''
+        sort = ''
+
+        for i in range(x):
+            if x[i] == 's':
+                query = x[i+1]
+            elif x[i] == 'p':
+                protein = x[i+1]
+            elif x[i] == 'f':
+                fats = x[i+1]
+            elif x[i] == 'c':
+                calories = x[i+1]
+            elif x[i] == 'a':
+                carbohydrates = x[i+1]
+            e
